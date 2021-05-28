c3ab249ea223cbb9386e99251ffcfd75a6e5e8ba
Lexam263 <73610041+Lexam263@users.noreply.github.com>
2021-05-28 15:29:18 -0500

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -2,10 +2,28 @@
 
 //variables
 
-let numA, 
-    numB, 
+let numA = 8, 
+    numB = "b", 
     total,
-    d7 = document.querySelector("#d7");
+    container = document.querySelector("#container"),
+    display = document.querySelector("#display"),
+    d7 = document.querySelector("#d7"),
+    d8 = document.querySelector("#d8"),
+    d9 = document.querySelector("#d9"),
+    ddivide = document.querySelector("#ddivide"),
+    d4 = document.querySelector("#d4"),
+    d5 = document.querySelector("#d5"),
+    d6 = document.querySelector("#d6"),
+    dmultiply = document.querySelector("#dmultiply"),
+    d1 = document.querySelector("#d1"),
+    d2 = document.querySelector("#d2"),
+    d3 = document.querySelector("#d3"),
+    dminus = document.querySelector("#dminus"),
+    d0 = document.querySelector("#d0"),
+    dclear = document.querySelector("#dclear"),
+    dequal = document.querySelector("#dequal"),
+    dplus = document.querySelector("#dplus");
+
 
 //Functions
 
@@ -33,8 +51,17 @@ function operator(numA, numB) {
     return add(numA, numB);
 }
 
+console.log(numA);
+
 d7.addEventListener('click', () => {
-    console.log("Button 7 works");
+    if (numA = "a") {
+        numA = 7;
+        console.log("numA " + numA); 
+    } else if(numB = "b") {
+        numB = 7;
+        console.log("numB " + numB);
+    }
+
 });
 
 /* console.log(add(4, 6));
@@ -42,4 +69,5 @@ console.log(subtract(6, 4));
 console.log(multiply(4, 6));
 console.log(divide(12, 4));
 console.log(operator(2, 3)); */
+
 
