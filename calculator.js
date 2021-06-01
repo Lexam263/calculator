dd6ad43fb946403bd7e68939a8510405e9d234ca
Lexam263 <73610041+Lexam263@users.noreply.github.com>
2021-05-31 22:30:42 -0500

    Buttons work now

--- calculator.js
+++ calculator.js
@@ -2,72 +2,88 @@
 
 //variables
 
-let numA = 8, 
-    numB = "b", 
-    total,
-    container = document.querySelector("#container"),
-    display = document.querySelector("#display"),
-    d7 = document.querySelector("#d7"),
-    d8 = document.querySelector("#d8"),
-    d9 = document.querySelector("#d9"),
-    ddivide = document.querySelector("#ddivide"),
-    d4 = document.querySelector("#d4"),
-    d5 = document.querySelector("#d5"),
-    d6 = document.querySelector("#d6"),
-    dmultiply = document.querySelector("#dmultiply"),
-    d1 = document.querySelector("#d1"),
-    d2 = document.querySelector("#d2"),
-    d3 = document.querySelector("#d3"),
-    dminus = document.querySelector("#dminus"),
-    d0 = document.querySelector("#d0"),
-    dclear = document.querySelector("#dclear"),
-    dequal = document.querySelector("#dequal"),
-    dplus = document.querySelector("#dplus");
-
+let array1 = [], 
+    string1 = '',
+    num1 = '',
+    array2 = [],
+    string2 = '',
+    num2 = '',
+    operand = '', 
+    total = '',
+    container = document.querySelector('#container'),
+    display = document.querySelector('#display'),
+    equal = document.querySelector('#equal'),
+    btnInts = document.querySelectorAll('btnInt'),
+    btnOperands = document.querySelectorAll('btnOperand'),
+    clear = document.querySelector('#clear');
 
 //Functions
 
-function add(numA, numB) {
-    total = (numA + numB)
-    return total;
-}
-
-function subtract(numA, numB) {
-    total = (numA - numB);
-    return total;
-}
-
-function multiply(numA, numB) {
-    total = (numA * numB);
-    return total;
-}
-
-function divide(numA, numB) {
-    total = (numA / numB);
-    return total;
-}
-
-function operator(numA, numB) {
-    return add(numA, numB);
-}
-
-console.log(numA);
-
-d7.addEventListener('click', () => {
-    if (numA = "a") {
-        numA = 7;
-        console.log("numA " + numA); 
-    } else if(numB = "b") {
-        numB = 7;
-        console.log("numB " + numB);
+btnInts.forEach((btnInt) => {
+    btnInt.addEventListener ('click', () => {
+        if (operand === '') { 
+            console.log("array1 " + btnInt.id); 
+            return array1.push(btnInt.id); 
+            } else {
+        console.log("array2 " + btnInt.id);
+        return array2.push(btnInt.id);
     }
-
+    });
 });
 
-/* console.log(add(4, 6));
-console.log(subtract(6, 4));
-console.log(multiply(4, 6));
-console.log(divide(12, 4));
-console.log(operator(2, 3)); */
+btnOperands.forEach((btnOperand) => {
+    btnOperand.addEventListener ('click', () => {
+        console.log(btnOperand.id);
+        if (operand === ""){
+            return string1 = array1.join(''), array2 = [],  operand = btnOperand.id;
+        } 
+        
+    });
+});
+
+equal.addEventListener('click', () => {
+    string2 = array2.join('');
+    num1 = parseInt(string1);
+    num2 = parseInt(string2);
+    if (operand === 'plus') {
+    total = (num1 + num2 + total);
+    } else if (operand === 'minus') {
+        total = (num1 - num2);
+    } else if (operand === 'multiply') {
+        total = (num1 * num2);
+    } else if (operand === 'divide') {
+        total = (num1 / num2);
+    }
+    console.log(num1);
+    console.log(operand);
+    console.log(num2);
+    console.log('total ' + total);
+    return array1.length = 0, 
+    array1[0] = total,
+    console.log("array1 " + array1),
+    string1 = '',
+    num1 = '',
+    array2 = [],
+    string2 = '',
+    num2 = '',
+    operand = '',
+    total = ''; 
+});
+
+clear.addEventListener('click', () => {
+    console.clear();
+    return array1 = [], 
+    string1 = '',
+    num1 = '',
+    array2 = [],
+    string2 = '',
+    num2 = '',
+    operand = '', 
+    total = '';
+})
 
 
+// Create a string of numbers
+// Add a mathematical sign
+//Create the next string of numbers
+// Execute the mathematical function.
