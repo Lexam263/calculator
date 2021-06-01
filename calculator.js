2c905ce5874cf7ea341e08737c8f8e61ddebccb9
Lexam263 <73610041+Lexam263@users.noreply.github.com>
2021-06-01 18:59:33 -0500

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -15,59 +15,99 @@ let array1 = [],
     equal = document.querySelector('#equal'),
     btnInts = document.querySelectorAll('btnInt'),
     btnOperands = document.querySelectorAll('btnOperand'),
+    element = document.getElementById("numDisplay"),
     clear = document.querySelector('#clear');
+    console.log("total in variables " + total);
 
 //Functions
 
 btnInts.forEach((btnInt) => {
-    btnInt.addEventListener ('click', () => {
+    btnInt.addEventListener ('click', (num1) => {
         if (operand === '') { 
-            console.log("array1 " + btnInt.id); 
-            return array1.push(btnInt.id); 
+            array1.push(btnInt.id); 
+            string1 = array1.join('');
+            console.log(string1);
+    
+        if(typeof(element) != 'undefined' && element != null) {
+                display.removeChild(element);
+                element = document.createElement("numDisplay");
+                element.textContent = string1; 
+                display.appendChild(element);
             } else {
-        console.log("array2 " + btnInt.id);
-        return array2.push(btnInt.id);
+                element = document.createElement("numDisplay");
+                element.textContent = string1; 
+                display.appendChild(element);
+            }
+            return string1;
+
+            } else {
+        array2.push(btnInt.id);
+        string2 = array2.join('');
+        console.log(string2);
+        if(typeof(element) != 'undefined' && element != null) {
+            display.removeChild(element);
+            element = document.createElement("numDisplay");
+            element.textContent = string2; 
+            display.appendChild(element);
+        } else {
+            element = document.createElement("numDisplay");
+            element.textContent = string2; 
+            display.appendChild(element);
+        }
+        return string2;
     }
+    
     });
 });
 
 btnOperands.forEach((btnOperand) => {
     btnOperand.addEventListener ('click', () => {
-        console.log(btnOperand.id);
         if (operand === ""){
-            return string1 = array1.join(''), array2 = [],  operand = btnOperand.id;
+            operand = btnOperand.id;
+            console.log(operand);
+            return array2 = [], operand;  
+
         } 
         
     });
 });
 
 equal.addEventListener('click', () => {
-    string2 = array2.join('');
     num1 = parseInt(string1);
     num2 = parseInt(string2);
     if (operand === 'plus') {
     total = (num1 + num2 + total);
+    console.log("total in plus " + total);
     } else if (operand === 'minus') {
         total = (num1 - num2);
+        console.log("total in minus " + total);
     } else if (operand === 'multiply') {
         total = (num1 * num2);
+        console.log("total in multiply " + total);
     } else if (operand === 'divide') {
         total = (num1 / num2);
+        console.log("total in divide " + total);
     }
-    console.log(num1);
-    console.log(operand);
-    console.log(num2);
-    console.log('total ' + total);
+    if(typeof(element) != 'undefined' && element != null) {
+        display.removeChild(element);
+        element = document.createElement("numDisplay");
+        element.textContent = total; 
+        display.appendChild(element);
+    } else {
+        element = document.createElement("numDisplay");
+        element.textContent = total; 
+        display.appendChild(element);
+    }
     return array1.length = 0, 
-    array1[0] = total,
-    console.log("array1 " + array1),
+    operand = '',
     string1 = '',
     num1 = '',
     array2 = [],
     string2 = '',
     num2 = '',
-    operand = '',
-    total = ''; 
+    array1[0] = total,
+    total = 0,
+    console.log("total in equal " + total);
 });
 
 clear.addEventListener('click', () => {
