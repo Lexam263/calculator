a86502adac1839858e531461c02245cf8c38a28b
Lexam263 <73610041+Lexam263@users.noreply.github.com>
2021-06-01 20:54:53 -0500

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -9,7 +9,7 @@ let array1 = [],
     string2 = '',
     num2 = '',
     operand = '', 
-    total = '',
+    total = 0,
     container = document.querySelector('#container'),
     display = document.querySelector('#display'),
     equal = document.querySelector('#equal'),
@@ -17,7 +17,6 @@ let array1 = [],
     btnOperands = document.querySelectorAll('btnOperand'),
     element = document.getElementById("numDisplay"),
     clear = document.querySelector('#clear');
-    console.log("total in variables " + total);
 
 //Functions
 
@@ -26,7 +25,6 @@ btnInts.forEach((btnInt) => {
         if (operand === '') { 
             array1.push(btnInt.id); 
             string1 = array1.join('');
-            console.log(string1);
     
         if(typeof(element) != 'undefined' && element != null) {
                 display.removeChild(element);
@@ -43,7 +41,6 @@ btnInts.forEach((btnInt) => {
             } else {
         array2.push(btnInt.id);
         string2 = array2.join('');
-        console.log(string2);
         if(typeof(element) != 'undefined' && element != null) {
             display.removeChild(element);
             element = document.createElement("numDisplay");
@@ -62,31 +59,52 @@ btnInts.forEach((btnInt) => {
 
 btnOperands.forEach((btnOperand) => {
     btnOperand.addEventListener ('click', () => {
-        if (operand === ""){
             operand = btnOperand.id;
-            console.log(operand);
             return array2 = [], operand;  
-
-        } 
-        
+        });
     });
-});
 
 equal.addEventListener('click', () => {
+    if (string1 != '') {
+        num1 = parseInt(string1);
+        if (operand === 'plus') {
+            total = (num1 + num1);
+            } else if (operand === 'minus') {
+                total = (num1 - num1);
+            } else if (operand === 'multiply') {
+                total = (num1 * num1);
+            } else if (operand === 'divide') {
+                total = (num1 / num1);
+            }
+            if(typeof(element) != 'undefined' && element != null) {
+                display.removeChild(element);
+                element = document.createElement("numDisplay");
+                element.textContent = total; 
+                display.appendChild(element);
+            } else {
+                element = document.createElement("numDisplay");
+                element.textContent = total; 
+                display.appendChild(element);
+            }
+            return array1.length = 0, 
+            string1 = total,
+            num1 = '',
+            array2 = [],
+            string2 = '',
+            num2 = '',
+            array1 = [],
+            total = 0;
+        } else {
     num1 = parseInt(string1);
     num2 = parseInt(string2);
     if (operand === 'plus') {
-    total = (num1 + num2 + total);
-    console.log("total in plus " + total);
+    total = (num1 + num2);
     } else if (operand === 'minus') {
         total = (num1 - num2);
-        console.log("total in minus " + total);
     } else if (operand === 'multiply') {
         total = (num1 * num2);
-        console.log("total in multiply " + total);
     } else if (operand === 'divide') {
         total = (num1 / num2);
-        console.log("total in divide " + total);
     }
     if(typeof(element) != 'undefined' && element != null) {
         display.removeChild(element);
@@ -99,15 +117,14 @@ equal.addEventListener('click', () => {
         display.appendChild(element);
     }
     return array1.length = 0, 
-    operand = '',
-    string1 = '',
+    string1 = total,
     num1 = '',
     array2 = [],
     string2 = '',
     num2 = '',
-    array1[0] = total,
-    total = 0,
-    console.log("total in equal " + total);
+    array1 = [],
+    total = 0;
+}
 });
 
 clear.addEventListener('click', () => {
@@ -119,11 +136,5 @@ clear.addEventListener('click', () => {
     string2 = '',
     num2 = '',
     operand = '', 
-    total = '';
+    total = 0;
 })
-
-
-// Create a string of numbers
-// Add a mathematical sign
-//Create the next string of numbers
-// Execute the mathematical function.
