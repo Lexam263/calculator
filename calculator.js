9588474cc13d078f24151a869cc6790f8aa0e1ea
Lexam263 <73610041+Lexam263@users.noreply.github.com>
2021-06-02 14:00:07 -0500

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -22,7 +22,7 @@ let array1 = [],
 
 btnInts.forEach((btnInt) => {
     btnInt.addEventListener ('click', (num1) => {
-        if (operand === '') { 
+        if (operand === '' || string1 === '') { 
             array1.push(btnInt.id); 
             string1 = array1.join('');
     
@@ -37,64 +37,55 @@ btnInts.forEach((btnInt) => {
                 display.appendChild(element);
             }
             return string1;
-
             } else {
         array2.push(btnInt.id);
         string2 = array2.join('');
         if(typeof(element) != 'undefined' && element != null) {
             display.removeChild(element);
             element = document.createElement("numDisplay");
-            element.textContent = string2; 
+            element.textContent = string1 + " " + symbol + " " + string2; 
             display.appendChild(element);
         } else {
             element = document.createElement("numDisplay");
-            element.textContent = string2; 
+            element.textContent = string1 + " " + symbol + " " + string2;  
             display.appendChild(element);
         }
         return string2;
     }
-    
     });
 });
 
 btnOperands.forEach((btnOperand) => {
     btnOperand.addEventListener ('click', () => {
             operand = btnOperand.id;
+            if (operand === "plus") {
+                symbol = "+";
+            } else if (operand === "minus") {
+                symbol = "-";
+            } else if (operand === "multiply") {
+                symbol = "*";
+            } else if (operand === "divide") {
+                symbol = "/";
+            } else { symbol = " "}
+
+            if(typeof(element) != 'undefined' && element != null) {
+                display.removeChild(element);
+                element = document.createElement("numDisplay");
+                element.textContent = string1 + " " + symbol; 
+                display.appendChild(element);
+            } else {
+                element = document.createElement("numDisplay");
+                element.textContent = string1 + " " + symbol; 
+                display.appendChild(element);
+            }
             return array2 = [], operand;  
         });
     });
 
 equal.addEventListener('click', () => {
-    if (string1 != '') {
-        num1 = parseInt(string1);
-        if (operand === 'plus') {
-            total = (num1 + num1);
-            } else if (operand === 'minus') {
-                total = (num1 - num1);
-            } else if (operand === 'multiply') {
-                total = (num1 * num1);
-            } else if (operand === 'divide') {
-                total = (num1 / num1);
-            }
-            if(typeof(element) != 'undefined' && element != null) {
-                display.removeChild(element);
-                element = document.createElement("numDisplay");
-                element.textContent = total; 
-                display.appendChild(element);
-            } else {
-                element = document.createElement("numDisplay");
-                element.textContent = total; 
-                display.appendChild(element);
-            }
-            return array1.length = 0, 
-            string1 = total,
-            num1 = '',
-            array2 = [],
-            string2 = '',
-            num2 = '',
-            array1 = [],
-            total = 0;
-        } else {
+    if (string1 != "" && total === 0) {
+              
+    } else {
     num1 = parseInt(string1);
     num2 = parseInt(string2);
     if (operand === 'plus') {
@@ -105,17 +96,17 @@ equal.addEventListener('click', () => {
         total = (num1 * num2);
     } else if (operand === 'divide') {
         total = (num1 / num2);
-    }
+    } 
     if(typeof(element) != 'undefined' && element != null) {
         display.removeChild(element);
         element = document.createElement("numDisplay");
-        element.textContent = total; 
+        element.textContent = string1 + " " + symbol + " " + string2 + " " + "=" + " " +  total; 
         display.appendChild(element);
     } else {
         element = document.createElement("numDisplay");
-        element.textContent = total; 
+        element.textContent = string1 + " " + symbol + " " + string2 + " " + "=" + " " +  total; 
         display.appendChild(element);
-    }
+    } }
     return array1.length = 0, 
     string1 = total,
     num1 = '',
@@ -124,11 +115,11 @@ equal.addEventListener('click', () => {
     num2 = '',
     array1 = [],
     total = 0;
-}
 });
 
 clear.addEventListener('click', () => {
     console.clear();
+    display.removeChild(element);
     return array1 = [], 
     string1 = '',
     num1 = '',
@@ -136,5 +127,7 @@ clear.addEventListener('click', () => {
     string2 = '',
     num2 = '',
     operand = '', 
+    element = document.getElementById("numDisplay"),
     total = 0;
+    
 })
